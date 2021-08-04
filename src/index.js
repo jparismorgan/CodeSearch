27b05c3e08614c7a5ffdcb804c87d394fb0961c5
Paris Morgan <1396242+jparismorgan@users.noreply.github.com>
2021-08-04 09:06:15 -0700

    back to flexsearch: added mark.js; tuning of flexsearch

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
 // import { useState, useEffect, useCallback } from 'react'
 // import Index from "./index.js";
 import FlexSearch from 'flexsearch'
-import lunr from 'lunr'
+import Mark from 'mark.js'
 
 import './styles.css'
 import data from './movies.js'
@@ -23,19 +23,22 @@ async function main() {
   const file1 = await getFile(file1Name)
   const file2 = await getFile(file2Name)
 
-  const builder = new lunr.Builder()
-  builder.metadataWhitelist.push('position')
-  builder.ref('id')
-  builder.field('content')
+  // const div1 = document.getElementById('file1')
+  // div1.textContent = file1
 
-  // var idx = lunr(function () {
-  //   this.ref('id')
-  //   this.field('body')
-  //   this.metadataWhitelist = ['position']
-
-  //   documents.forEach(function (doc) { this.add(doc) }, this)
-  // })
-
+  const index = new FlexSearch.Index({
+    // charset: 'latin:advanced',
+    tokenize: 'full',
+    cache: true,
+    resolution: 9, // 9 is default
+    context: true,
+    // the default encoder removes special characters. define a new encoder that just splits on spaces.
+    // https://github.com/nextapps-de/flexsearch/issues/197
+    // https://github.com/nextapps-de/flexsearch/issues/145
+    encode: function (str) {
+      return str.split(/\s+/g)
+    }
+  })
   const fileNameToContent = {}
   fileNameToContent[file1Name] = file1
   fileNameToContent[file2Name] = file2
@@ -52,47 +55,154 @@ async function main() {
     content.split('\n').forEach((line) => {
       const tagId = `${tag}-${id++}`
       tagIdToLine[tagId] = line
-      // index.add(tagId, line)
-      builder.add({ id: tagId, content: line })
+      index.add(tagId, line)
     })
   })
-  const index = builder.build()
 
   var suggestions = document.getElementById('suggestions')
   var autocomplete = document.getElementById('autocomplete')
   var userinput = document.getElementById('userinput')
 
   userinput.addEventListener('input', show_results, true)
+  userinput.addEventListener('keyup', accept_autocomplete, true)
+  suggestions.addEventListener('click', accept_suggestion, true)
+
+  // HighlightMatch('test string', 't')
+
+  function createElementFromHTML(htmlString) {
+    var div = document.createElement('div')
+    div.innerHTML = htmlString
+    // console.log('div.innerHTML:', div.innerHTML)
+
+    // Change this to div.childNodes to support multiple top-level nodes
+    // return div.firstChild;
+    return div
+  }
+
+  // suggestions.appendChild(createElementFromHTML(HighlightMatch('<test>', '<t')))
+  // suggestions.appendChild(createElementFromHTML(highlight('test<?test>', 't')))
+
+  function highlight(content, keyword) {
+    const newKeyword = `<mark>${keyword}</mark>`
+    const resContent = content.replace(keyword, newKeyword)
+
+    console.log('----')
+    console.log('keyword: ', keyword)
+    console.log('content: ', content)
+    console.log('resContent: ', resContent)
+
+    return resContent
+
+    // Content would be
+    // &lt;<em>audio</em>&gt;: The Embed <em>Audio</em> element
+  }
+
+  function highlight1(content, keyword) {
+    // const keyword = 'audio';
+    // const content = '&lt;audio&gt;: The Embed Audio element'
+
+    // Make sure there's no regex special chars
+    const sanitizedKeyword = keyword.replace(/\W/g, '')
+
+    // Build regex
+    const regexForContent = new RegExp(sanitizedKeyword, 'gi')
+
+    // Replace content where regex matches
+    // const resContent = content.replace(regexForContent, '<em>$&</em>');
+    const resContent = content.replace(regexForContent, '<mark>$&</mark>')
+
+    console.log('----')
+    console.log('keyword: ', keyword)
+    console.log('content: ', content)
+    console.log('sanitizedKeyword: ', sanitizedKeyword)
+    console.log('resContent: ', resContent)
+
+    return resContent
+
+    // Content would be
+    // &lt;<em>audio</em>&gt;: The Embed <em>Audio</em> element
+  }
+
+  function HighlightMatch(title, q) {
+    // FlexSearch doesn't support finding out which "typo corrections"
+    // were done unfortunately.
+    // See https://github.com/nextapps-de/flexsearch/issues/99
+
+    // Split on higlight term and include term into parts, ignore case.
+    const words = q.trim().toLowerCase().split(/[ ,]+/)
+    console.log(title, q, words)
+
+    // $& means the whole matched string
+    const regexWords = words.map((s) =>
+      s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+    )
+    const regex = `\\b(${regexWords.join('|')})`
+    const parts = title.split(new RegExp(regex, 'gi'))
+
+    let ret = ''
+    parts.map((part, i) => {
+      const key = `${part}:${i}`
+      if (words.includes(part.toLowerCase())) {
+        // console.log(`<mark key=${key}>${part}</mark>`)
+        ret = ret.concat(`<mark key=${key}>${part}</mark>`)
+      } else {
+        // console.log(`<span key=${key}>${part}</span>`)
+        ret = ret.concat(`<span key=${key}>${part}</span>`)
+      }
+    })
+    console.log(ret)
+    return ret
+
+    // for a node component
+    // return (
+    //   <b>
+    //     {parts.map((part, i) => {
+    //       const key = `${part}:${i}`
+    //       if (words.includes(part.toLowerCase())) {
+    //         return <mark key={key}>{part}</mark>
+    //       } else {
+    //         return <span key={key}>{part}</span>
+    //       }
+    //     })}
+    //   </b>
+    // )
+  }
 
   function show_results() {
-    console.log(' ----- [show_results] value: ', this.value)
+    console.log('[show_results] value: ', this.value)
     var value = this.value
-    // var results = index.search(value, 25, { suggest: true })
-    var results = index.search(value)
-
+    var results = index.search(value, { suggest: true, limit: 10 })
     var entry = null
     var childs = suggestions.childNodes
     var i = 0
     var len = results.length
-
+    console.log(results)
     for (; i < len; i++) {
-      // console.log(results[i].matchData.metadata.a.content.position[0],tagIdToLine[results[i].ref])
       entry = childs[i]
-      console.log(results[i].ref, tagIdToLine[results[i].ref], results[i].score)
+
       if (!entry) {
         entry = document.createElement('div')
+        // entry = createElementFromHTML(
+        // // HighlightMatch(tagIdToLine[results[i]], value)
+        // highlight(tagIdToLine[results[i]], value)
+        // )
         suggestions.appendChild(entry)
       }
 
-      entry.textContent = tagIdToLine[results[i].ref]
-      // entry.innerHTML = highlight(entry, results[i].matchData.metadata.a.content.position)
+      const file
+      entry.textContent = tagIdToLine[results[i]]
+      // entry.innerHTML =
+      //   // HighlightMatch(tagIdToLine[results[i]], value)
+      //   highlight(tagIdToLine[results[i]], value)
+      var instance = new Mark(entry)
+      instance.mark(value)
     }
 
     while (childs.length > len) {
       suggestions.removeChild(childs[i])
     }
 
-    var first_result = results[0] && tagIdToLine[results[0].ref]
+    var first_result = tagIdToLine[results[0]]
     var match =
       first_result && first_result.toLowerCase().indexOf(value.toLowerCase())
 
@@ -102,84 +212,24 @@ async function main() {
     } else {
       autocomplete.value = autocomplete.current = value
     }
-
-    // highlight(suggestions, results)
   }
 
-  /**
-   * Represents the location of a match within a
-   * larger string. Extracted from a lunr.Index~Result.
-   *
-   * @typedef {number[]} MatchLocation
-   * @property {number} 0 - Starting index of the match
-   * @property {number} 1 - Length of the match
-   */
+  function accept_autocomplete(event) {
+    if ((event || window.event).keyCode === 13) {
+      this.value = autocomplete.value = autocomplete.current
+    }
+  }
 
-  /**
-   * Highlights text within a dom element.
-   *
-   * Specifically this is designed to work with the output
-   * positions of terms returned from a lunr search.
-   *
-   * @param {HTMLElement} element - the element that contains text to highlight.
-   * @param {MatchLocation[]} matches - the list of matches to highlight.
-   */
-  function highlight(element, matches) {
-    var nodeFilter = {
-      acceptNode: function (node) {
-        if (/^[\t\n\r ]*$/.test(node.nodeValue)) {
-          return NodeFilter.FILTER_SKIP
-        }
-        return NodeFilter.FILTER_ACCEPT
-      }
+  function accept_suggestion(event) {
+    var target = (event || window.event).target
+
+    userinput.value = autocomplete.value = target.textContent
+
+    while (suggestions.lastChild) {
+      suggestions.removeChild(suggestions.lastChild)
     }
 
-    var index = 0,
-      matches = matches
-        .sort(function (a, b) {
-          return a[0] - b[0]
-        })
-        .slice(),
-      previousMatch = [-1, -1]
-    ;(match = matches.shift()),
-      (walker = document.createTreeWalker(
-        element,
-        NodeFilter.SHOW_TEXT,
-        nodeFilter,
-        false
-      ))
-    console.log(matches)
-    while ((node = walker.nextNode())) {
-      if (match == undefined) break
-      if (match[0] == previousMatch[0]) continue
-
-      var text = node.textContent,
-        nodeEndIndex = index + node.length
-
-      if (match[0] < nodeEndIndex) {
-        var range = document.createRange(),
-          tag = document.createElement('mark'),
-          rangeStart = match[0] - index,
-          rangeEnd = rangeStart + match[1]
-
-        tag.dataset.rangeStart = rangeStart
-        tag.dataset.rangeEnd = rangeEnd
-
-        range.setStart(node, rangeStart)
-        range.setEnd(node, rangeEnd)
-        range.surroundContents(tag)
-
-        index = match[0] + match[1]
-
-        // the next node will now actually be the text we just wrapped, so
-        // we need to skip it
-        walker.nextNode()
-        previousMatch = match
-        match = matches.shift()
-      } else {
-        index = nodeEndIndex
-      }
-    }
+    return false
   }
 }
 
